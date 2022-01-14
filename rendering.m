e5479312758ed9ffb15105a8ff07829ee89d7a30
Baptiste Brument <51319846+bbrument@users.noreply.github.com>
2022-01-14 18:04:21 +0100

    with/out albedo

--- rendering.m
+++ rendering.m
@@ -7,6 +7,17 @@ display_ = 1;
 dataPath = 'data/bivar_gaussian_200_with_albedo/';
 mkdir(dataPath);
 
+% Rendering options
+repCam = 255;
+scale = 5;
+imageSize = 1000/scale;
+
+% Calibration matrix
+px = 500/scale;
+py = 500/scale;
+f = 1500/scale;
+K = [f 0 px ; 0 f py ; 0 0 1];
+
 % Geometry
 %sigma = 0.5;
 %zFunc = @(X,Y) 1/(sigma*sqrt(2*pi)).*exp(-(X.^2/(2*sigma^2)));
@@ -18,11 +29,9 @@ zFunc = @(X,Y) 1/(sigma(1)*sigma(2)*sqrt(2*pi)).*exp(-1/2*((X-mu(1)).^2/sigma(1)
 % Albedo
 albedoSize = 500;
 albedoRange = [1 albedoSize];
-im = double(rgb2gray(imread('peppers.png')));
+im = double(rgb2gray(imread('peppers.png')))/repCam; % with albedo
+%im = 1; % without albedo
 im = imresize(im,[albedoSize albedoSize]);
-% albedoFunc = @(X,Y) round(cat(3,interp2(albedoRange,albedoRange,im(:,:,1),X,Y,'cubic'), ...
-%     interp2(albedoRange,albedoRange,im(:,:,2),X,Y,'cubic'), ...
-%     interp2(albedoRange,albedoRange,im(:,:,3),X,Y,'cubic')));
 albedoBinFunc = @(X,Y) interp2(im,mapToRange(X,geomRange,albedoRange),mapToRange(Y,geomRange,albedoRange),...
     'cubic',0);
 
@@ -31,7 +40,7 @@ if display_
     y = x;
     [X,Y] = meshgrid(x,y);
     figure; hold on;
-    surf(X,Y,zFunc(X,Y),uint8(albedoBinFunc(X,Y)));
+    surf(X,Y,zFunc(X,Y),uint8(repCam*albedoBinFunc(X,Y)));
     shading interp
     axis tight
     axis equal
@@ -41,13 +50,6 @@ end
 normalsFunc = @(X,Y) [-(2^(1/2).*exp(-(mu(1) - X).^2/(2*sigma(1)^2) - (mu(2)-Y).^2/(2*sigma(2)^2)).*(2*mu(1) - 2*X))/(4*pi^(1/2)*sigma(1)^4);
     -(2^(1/2).*exp(- (mu(1) - X).^2/(2*sigma(1)^2) - (mu(2) - Y).^2/(2*sigma(2)^2)).*(2*mu(2) - 2*Y))/(4*pi^(1/2)*sigma(1)^2*sigma(2)^2);
     ones(1,size(X,2))];
-
-% Calibration matrix
-scale = 5;
-px = 500/scale;
-py = 500/scale;
-f = 1500/scale;
-K = [f 0 px ; 0 f py ; 0 0 1];
 
 % Multi view & multi light
 intLight = 1;
@@ -79,8 +81,6 @@ for i = 1:nCameras
 end
 
 % Rendering
-repCam = 1;
-imageSize = 1000/scale;
 for i = 1:nCameras
     disp([ 'View : ' int2str(i) ]);
     RCam = RCamTab(:,:,i); tCam = -RCam*centerCamTab(:,i);
@@ -89,11 +89,15 @@ for i = 1:nCameras
         imwrite(renderedImages(:,:,:,j),[ dataPath 'light_' sprintf('%02d/',j) ...
             'image_' sprintf('%02d',i) '.png' ]);
         imwrite(renderedImages(:,:,:,j),[ dataPath 'view_' sprintf('%02d/',i) ...
-            'image_' sprintf('%02d',j) '.png' ]);
+            'image_' sprintf('%02d',j) '.png' ]);s
     end
+
     if display_
-        figure;
+        if i == 1
+            figure;
+        end
+        subplot(3,3,i);
         imshow(renderedImages(:,:,:,5));
+        title([ 'View ' sprintf('%02d',i) ]);  
     end
 end
-
